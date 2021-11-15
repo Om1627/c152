17ac2a00248f2508fbf997dfc151f60d05270e13
Om1627 <60969676+Om1627@users.noreply.github.com>
2021-11-15 19:16:12 +0530

    1

--- car.js
+++ car.js
@@ -1,6 +1,7 @@
 AFRAME.registerComponent("car-model", {
     schema: {
-      modelRef: { type: "string", default: "../assets/car/scene.gltf" }
+      modelRef: { type: "string", default: "../assets/car/scene.gltf" },
+      clickCounter:{type:"number",default:0}
     },
     init: function() {
       // Do something when component first attached.
@@ -10,5 +11,19 @@ AFRAME.registerComponent("car-model", {
       this.el.setAttribute("position", position);
       this.el.setAttribute("rotation", rotation);
       this.el.setAttribute("scale",{x:0.5,y:0.5,z:0.5})
+    },
+    update:function(){
+      window.addEventListener("click",e=>{
+        this.data.clickCounter=this.data.clickCounter+1;
+        if(this.data.clickCounter===1){
+          const rotation={x:0,y:20,z:0};
+          this.el.setAttribute("rotation",rotation);
+        }
+        else if(this.data.clickCounter===2){
+          const rotation = {x:0,y:100,z:0}
+          this.data.clickCounter=0
+        }
+      })
+      
     }
   });
